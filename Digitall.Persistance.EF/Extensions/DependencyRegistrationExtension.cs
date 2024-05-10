3b2185290411ca936e23e1e9876360567b6e759c
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-10 20:37:49 +0300

    [EF] Add DB Context and EF DI ext

---
+++ Digitall.Persistance.EF/Extensions/DependencyRegistrationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Digitall.Persistance.EF.Extensions
+{
+    public static class DependencyRegistrationExtension
+    {
+        public static IServiceCollection AddPersistanceEF(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDbContext<WarehouseDbContext>(
+                    opt => opt.UseSqlServer(configuration.GetConnectionString("WarehouseConnection")));
+
+            return services;
+        }
+    }
+}
---
+++ Digitall.Persistance.EF/WarehouseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Digitall.Warehouse.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digitall.Persistance.EF
+{
+    public class WarehouseDbContext : DbContext
+    {
+        public WarehouseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+    }
+}
