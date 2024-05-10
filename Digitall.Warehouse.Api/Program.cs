0944ff0bf6d1751adcec7028f4408b8073492985
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-10 20:38:39 +0300

    [API] Register EF Persistance

--- Digitall.Warehouse.Api/Program.cs
+++ Digitall.Warehouse.Api/Program.cs
@@ -1,8 +1,12 @@
+using Digitall.Persistance.EF.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddPersistanceEF(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
