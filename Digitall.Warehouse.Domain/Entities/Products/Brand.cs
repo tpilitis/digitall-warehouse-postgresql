c39830bad510b595f91f068519838763491c9826
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-13 19:16:12 +0300

    [Domain] Removed obsolete attributes

--- Digitall.Warehouse.Domain/Entities/Products/Brand.cs
+++ Digitall.Warehouse.Domain/Entities/Products/Brand.cs
@@ -7,8 +7,6 @@ public class Brand : IIdentifiable<Guid>
 {
     public Guid Id { get; }
 
-    [Required]
-    [MaxLength(255)]
     public string? Name { get; set; }
 
     public ICollection<Product> Products { get; set; } = new List<Product>();
--- Digitall.Warehouse.Domain/Entities/Products/Price.cs
+++ Digitall.Warehouse.Domain/Entities/Products/Price.cs
@@ -1,8 +1,3 @@
 ﻿namespace Digitall.Warehouse.Domain.Entities.Products;
 
-/// <summary>
-/// Price complext type.
-/// </summary>
-/// <param name="Value">Price Value</param>
-/// <param name="CurrencyCode">ISO Currency code</param>
 public record Price(decimal Value, string CurrencyCode);
