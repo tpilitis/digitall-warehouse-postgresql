f44be9eaf8d06c634cc0a5babf4b5299c870f06a
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-14 16:28:27 +0300

    [EF] Add product title index

---
+++ Digitall.Persistance.EF/Migrations/20240514132807_AddProductTitleIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digitall.Persistance.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTitleIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Title",
+                table: "Products",
+                column: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Title",
+                table: "Products");
+        }
+    }
+}
