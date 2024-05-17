e4aea63caea030b4b08b911f70a8dc4de68f78c1
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-17 21:03:19 +0300

    [Utils] Add utils project
    * define Result;
    * define Error;

---
+++ Digital.Warehouse.Utils/Error.cs
@@ -0,0 +1,12 @@
+﻿namespace Digital.Warehouse.Utils
+{
+    public sealed record Error(string Code, string Description)
+    {
+        public static readonly Error None = new Error(string.Empty, string.Empty);
+        public static readonly Error NullValue = new Error("Error.NullValue", "Null value was provided.");
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+
+        public Result ToResult() => Result.Failure(this);
+    }
+}
