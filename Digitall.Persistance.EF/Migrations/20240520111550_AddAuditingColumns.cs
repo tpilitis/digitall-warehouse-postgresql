a617027ace57bc3c7cddf6b96cbcf7b3f369d896
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-20 14:42:59 +0300

    [EF] Add migration for auditing columns

---
+++ Digitall.Persistance.EF/Migrations/20240520111550_AddAuditingColumns.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digitall.Persistance.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditingColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Variants",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Variants",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Swatches",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Swatches",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Sizes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Sizes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ProductCategory",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "ProductCategory",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Brands",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Brands",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Variants");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Variants");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Swatches");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Swatches");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Sizes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Sizes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "ProductCategory");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "ProductCategory");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Brands");
+        }
+    }
+}
