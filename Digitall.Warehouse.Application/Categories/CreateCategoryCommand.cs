037087dbfa79d40a70cf5b9e2fd9eb9740fb4547
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-20 13:11:27 +0300

    [Application] Create Category Command

---
+++ Digitall.Warehouse.Application/Categories/CreateCategoryCommand.cs
@@ -0,0 +1,7 @@
+﻿
+using Digitall.Warehouse.Application.Abstractions.Messaging;
+
+namespace Digitall.Warehouse.Application.Categories
+{
+    public sealed record CreateCategoryCommand(string Name) : ICommand;
+}
---
+++ Digitall.Warehouse.Application/Categories/CreateCategoryCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Digitall.Warehouse.Application.Abstractions.Messaging;
+using Digitall.Warehouse.Application.Abstractions.Persistence;
+using Digitall.Warehouse.Domain.Entities.Products;
+using Digitall.Warehouse.Domain.Shared;
+
+namespace Digitall.Warehouse.Application.Categories
+{
+    public class CreateCategoryCommandHandler : ICommandHandler<CreateCategoryCommand>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateCategoryCommandHandler(
+            ICategoryRepository categoryRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _categoryRepository = categoryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(
+            CreateCategoryCommand request,
+            CancellationToken cancellationToken)
+        {
+            var existingCategory = await _categoryRepository.GetByNameAsync(request.Name);
+            if (existingCategory != null)
+            {
+                // category must be unique
+                return Result.Failure(Error.DuplicatedValue(nameof(request.Name)));
+            }
+
+            var category = new Category()
+            {
+                Name = request.Name,
+            };
+
+            await _categoryRepository.AddAsync(category, cancellationToken);
+            await _unitOfWork.SaveChangesAsync();
+
+            return Result.Success(category);
+        }
+    }
+}
