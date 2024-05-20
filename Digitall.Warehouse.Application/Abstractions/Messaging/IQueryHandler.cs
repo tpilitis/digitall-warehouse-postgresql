7e7348256a6b37ffa72d70c442a44f9297fe8189
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-20 14:43:16 +0300

    [App] Implement Query abstraction

---
+++ Digitall.Warehouse.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using Digitall.Warehouse.Domain.Shared;
+using MediatR;
+
+namespace Digitall.Warehouse.Application.Abstractions.Messaging;
+
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+}
