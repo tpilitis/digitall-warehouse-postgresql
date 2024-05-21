dfd9be2db8f9de87fb3d949261f46bb9cf8b7b5c
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-21 16:31:07 +0300

    [API] Introduce SeriLogger with 2lvl init

--- Digitall.Warehouse.Api/Program.cs
+++ Digitall.Warehouse.Api/Program.cs
@@ -6,8 +6,27 @@ using Digitall.Warehouse.Application.Behaviors;
 using Digitall.Warehouse.Application.Categories.Commands;
 using FluentValidation;
 using MediatR;
+using Serilog;
+using Serilog.Events;
+
+// seriLog 2lv initialisation
+// require this so we can capture issues during the set up
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// second level serilog initialisation
+builder.Services.AddSerilog((serviceProvider, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(serviceProvider)
+        .Enrich.FromLogContext();
+});
 
 // Add services to the container.
 builder.Services.AddPersistanceEF(builder.Configuration);
