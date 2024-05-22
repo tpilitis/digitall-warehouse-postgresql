b75b859f3f0f2e102ed7681e6cf3263dc7edec39
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-22 14:23:57 +0300

    [EF] Use the new namespace format

--- Digitall.Persistance.EF/UnitOfWork.cs
+++ Digitall.Persistance.EF/UnitOfWork.cs
@@ -2,33 +2,32 @@
 using Digitall.Warehouse.Domain.Abstraction;
 using Microsoft.EntityFrameworkCore;
 
-namespace Digitall.Persistance.EF
+namespace Digitall.Persistance.EF;
+
+public class UnitOfWork(WarehouseDbContext dbContext) : IUnitOfWork
 {
-    public class UnitOfWork(WarehouseDbContext dbContext) : IUnitOfWork
+    private readonly WarehouseDbContext _dbContext = dbContext;
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        private readonly WarehouseDbContext _dbContext = dbContext;
+        UpdateAuditableEntities();
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
 
-        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    private void UpdateAuditableEntities()
+    {
+        var auditableEntries = _dbContext.ChangeTracker.Entries<IAuditable>();
+
+        foreach (var auditableEntry in auditableEntries)
         {
-            UpdateAuditableEntities();
-            await _dbContext.SaveChangesAsync(cancellationToken);
-        }
-
-        private void UpdateAuditableEntities()
-        {
-            var auditableEntries = _dbContext.ChangeTracker.Entries<IAuditable>();
-
-            foreach (var auditableEntry in auditableEntries)
+            if (auditableEntry.State == EntityState.Added)
             {
-                if (auditableEntry.State == EntityState.Added)
-                {
-                    auditableEntry.Property(entity => entity.CreatedAt).CurrentValue = DateTime.UtcNow;
-                }
+                auditableEntry.Property(entity => entity.CreatedAt).CurrentValue = DateTime.UtcNow;
+            }
 
-                if (auditableEntry.State == EntityState.Modified)
-                {
-                    auditableEntry.Property(entity => entity.ModifiedAt).CurrentValue = DateTime.UtcNow;
-                }
+            if (auditableEntry.State == EntityState.Modified)
+            {
+                auditableEntry.Property(entity => entity.ModifiedAt).CurrentValue = DateTime.UtcNow;
             }
         }
     }
