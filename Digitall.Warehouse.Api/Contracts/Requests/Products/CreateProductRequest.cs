94a3c86750687517bf836d1710a6f60a2387b0be
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-23 14:43:22 +0300

    [API] Init Product Controller

---
+++ Digitall.Warehouse.Api/Contracts/Requests/Products/CreateProductRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Digitall.Warehouse.Api.Contracts.Requests.Products;
+
+public class CreateProductRequest
+{
+    public string Title { get; set; } = default!;
+
+    public string Descritpion { get; set; } = default!;
+
+    public Money Price { get; set; } = default!;
+
+    public Guid CategoryId { get; set; } = default!;
+
+    public Guid? Brand { get; set; } = default!;
+}
---
+++ Digitall.Warehouse.Api/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Digitall.Warehouse.Api.Contracts.Requests.Products;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Digitall.Warehouse.Api.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ProductsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<IAsyncResult> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [HttpPost]
+        public async Task<IActionResult> CreateProductAsync([FromBody] CreateProductRequest createProductRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
