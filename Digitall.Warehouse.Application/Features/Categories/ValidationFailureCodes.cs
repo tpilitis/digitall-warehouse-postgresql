6506c9a0ae795d79ce837703dc4bc2ff64a1d9f2
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-27 14:57:12 +0300

    [App] Add forgotten code

--- Digitall.Warehouse.Application/Features/Categories/ValidationFailureCodes.cs
+++ Digitall.Warehouse.Application/Features/Categories/ValidationFailureCodes.cs
@@ -7,6 +7,7 @@ public class ValidationFailureCodes : FailureCode<ValidationFailureCodes>
     public static readonly ValidationFailureCodes CategoryNameAlreadyExists = new(nameof(CategoryNameAlreadyExists), "Category name must be unique.");
     public static readonly ValidationFailureCodes CategoryNotFound = new(nameof(CategoryNotFound), "Category cannot be found.");
     public static readonly ValidationFailureCodes EntityDoesNotExists = new(nameof(EntityDoesNotExists), "Entity does not exists");
+    public static readonly ValidationFailureCodes IdentifierCannotBeEmpty = new(nameof(IdentifierCannotBeEmpty), "Identifier cannot be empty");
 
     public ValidationFailureCodes(string code, string message)
     : base(code, message)
