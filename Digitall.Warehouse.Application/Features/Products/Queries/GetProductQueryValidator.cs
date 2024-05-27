072590fa368e019303f383bb2c471fd9454d0576
Teodora Pilitis <teodora.pilitis@gmail.com>
2024-05-27 14:56:33 +0300

    [App] Add query validator

---
+++ Digitall.Warehouse.Application/Features/Products/Queries/GetProductQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Digitall.Warehouse.Application.Features.Categories;
+using FluentValidation;
+
+namespace Digitall.Warehouse.Application.Features.Products.Queries
+{
+    public class GetProductQueryValidator : AbstractValidator<GetProductQuery>
+    {
+        public GetProductQueryValidator()
+        {
+            RuleFor(query => query.ProductId)
+                .Must((productId) =>
+                {
+                    return !productId.Equals(Guid.Empty);
+                })
+                .WithErrorCode(ValidationFailureCodes.IdentifierCannotBeEmpty.Name)
+                .WithMessage(ValidationFailureCodes.IdentifierCannotBeEmpty.Value);
+        }
+    }
+}
